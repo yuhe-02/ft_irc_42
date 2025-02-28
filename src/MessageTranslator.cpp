e6869cb3c729a74ad8eca92491d2c9aed18164cf
tashiget <tashiget@student.42tokyo.jp>
2025-02-28 21:03:26 +0900

    test

--- src/MessageTranslator.cpp
+++ src/MessageTranslator.cpp
@@ -207,7 +207,7 @@ void MessageTranslator::OutputLog()
 
 ChannelResult	MessageTranslator::Exit(std::vector<std::string>, int player_fd)
 {
-	if (everyone_id_.find(player_fd) == everyone_id_.end())
+	if (!Everyone::GetInstance()->IsRegister(player_fd))
 		return (ChannelResult(FATAL, ""));
 	if (Everyone::GetInstance()->GetSomeone(player_fd).is_admin)
 		exit(0);
