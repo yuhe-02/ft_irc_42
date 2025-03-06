09ccf850ece756dd46e5c8727a867b88ac156f2a
yuhe-02 <domineter4649@gmail.com>
2025-03-06 14:49:13 +0000

    [FIX] refactor Sender class method

--- src/Sender.cpp
+++ src/Sender.cpp
@@ -1,17 +1,5 @@
 #include "Sender.hpp"
 
-static std::string zfill(int num, int digit)
-{
-    std::stringstream ss;
-    ss << num;
-    std::string str = ss.str();
-
-    while (str.length() < static_cast<size_t>(digit))
-    {
-        str = "0" + str;
-    }
-    return str;
-}
 Sender::Sender(void)
 {
 
@@ -33,21 +21,15 @@ Sender& Sender::operator=(const Sender& src)
 	return (*this);
 }
 
-void	Sender::SendMessage(ChannelResult result, int fd) const
+std::string Sender::createResponse(ChannelResult &result, int fd) const
 {
-	ssize_t		byte_send;
 	std::string	message;
-
-	if (fd < 0)
-	{
-		std::cerr << "Error: send" << std::endl;
-		return ;
-	}
 	std::stringstream ss;
+
 	if (result.first != -1)
 	{
 		// std::cerr << "ChannelResult: " << result.first << ", " << result.second << std::endl;//デバッグ用
-		ss << ":localhost " << zfill(result.first, 3);
+		ss << ":localhost " << std::setw(3) << std::setfill('0') << result.first;
 		if ((result.first != 451 && result.first != 464) && (result.second.find("PASS") == std::string::npos))
 		{
 			// unknown command時にニックネームがない可能性があるため
@@ -64,15 +46,38 @@ void	Sender::SendMessage(ChannelResult result, int fd) const
 	{
 		ss << result.second;
 	}
-	if (!ss.str().empty())
-	{
-		ss << "\r\n";
-	}
-	std::cerr << "Message: " << ss.str();//デバッグ用
-	// message += "\r\n";
-	byte_send = send(fd, ss.str().c_str(), ss.str().length(), 0);
-	if (byte_send == -1)
+	ss << "\r\n";
+	std::string msg = ss.str();
+	return (msg);
+}
+
+ssize_t Sender::sendResponse(std::string &response, int fd) const
+{
+    ssize_t bytes_sent = 0;
+    size_t total_len = response.length();
+	// 一回のメッセージ送信で全て送れるとは限らないらしく、送れるまでループ
+    while (bytes_sent < static_cast<ssize_t>(total_len))
+    {
+        ssize_t sent = send(fd, response.c_str() + bytes_sent, total_len - bytes_sent, 0);
+        if (sent == -1)
+        {
+            std::cerr << "Error: send" << std::endl;
+            return (-1);
+        }
+        bytes_sent += sent;
+    }
+	return (bytes_sent);
+}
+
+void	Sender::SendMessage(ChannelResult result, int fd) const
+{
+	if (fd < 0)
 	{
 		std::cerr << "Error: send" << std::endl;
+		return ;
 	}
+    std::string response = createResponse(result, fd);
+	// デバッグ用メッセージ
+    std::cerr << "Message: " << response;
+	sendResponse(response, fd);
 }
